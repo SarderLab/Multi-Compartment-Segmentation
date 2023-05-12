d8345e2aa92ebc7b3817d18e98df449061ee9c6a
sayatmimar <sayat.mimar@ufl.edu>
2023-05-12 13:45:54 -0400

    add updates

--- histomicstk/cli/MultiCompartmentSegment/MultiCompartmentSegment.py
+++ histomicstk/cli/MultiCompartmentSegment/MultiCompartmentSegment.py
@@ -1,31 +1,64 @@
 import os
 import sys
-# from glob import glob
+import girder_client
+import torch
+import tiffslide as openslide
 from histomicstk.cli.utils import CLIArgumentParser
 
 
 def main(args):
 
+    
+
+    folder = args.base_dir
+    base_dir_id = folder.split('/')[-2]
+    _ = os.system("printf '\nUsing data from girder_client Folder: {}\n'".format(folder))
+
+
+    # newfile = gc.createFolder(girder_folder_id,'tes_file_with_girder')
+
+    
+
+    
+
+    _ = os.system("printf '\n---\n\nFOUND: [{}]\n'".format(args.input_file))
+    print(os.path.getsize(args.input_file))
+    slide=openslide.TiffSlide(args.input_file)
+
+    print('yess')
+
+
+    WSIs = [slide]
+
+
     cwd = os.getcwd()
-
     print(cwd)
+    os.chdir(cwd)
 
+
+    
+    modelfile = torch.load(args.modelfile)
+    torch.save(modelfile)
+    os.system('pwd')
+    os.system('ls -lh')
     # tmp = args.outputAnnotationFile
     # tmp = os.path.dirname(tmp)
     # #print(tmp)
 
+    
+    
     #os.makedirs('test_dir')
-    os.chdir(cwd)
+    
     # os.makedirs('test_dir_1')
     # try:
     #     os.makedirs(cwd+'/test_dir_1')
     # except:
     #     print('yay')
     #model = glob('{}/*.pth'.format(tmp))[0]
-
+    
     #print(model)
     #print('\noutput filename: {}\n'.format(args.outputAnnotationFile))
-    cmd = "python3 ../segmentationschool/segmentation_school.py --option {} --project {} --base_dir {} --modelfile {}".format(args.option, args.project, args.base_dir, args.modelfile)
+    cmd = "python3 ../segmentationschool/segmentation_school.py --option {} --base_dir {} --modelfile {} --girderApiUrl {} --girderToken {} --files {}".format(args.option, args.base_dir, modelfile, args.girderApiUrl, args.girderToken,WSIs)
     print(cmd)
     sys.stdout.flush()
     os.system(cmd)
--- histomicstk/segmentationschool/Codes/InitializeFolderStructure.py
+++ histomicstk/segmentationschool/Codes/InitializeFolderStructure.py
@@ -1,24 +1,24 @@
-import os
+import os,girder_client
 import numpy as np
 from glob import glob
-from shutil import rmtree,copy#move,copyfile
+from shutil import rmtree,copy#,move,copyfile
 
 def purge_training_set(args):
-    rmtree(args.project + '/Permanent/')
-    rmtree(args.project + '/TempHR')
-    rmtree(args.project + '/TempLR')
+    rmtree(args.base_dir + '/' + args.project + '/Permanent/')
+    rmtree(args.base_dir + '/' + args.project + '/TempHR/')
+    rmtree(args.base_dir + '/' + args.project + '/TempLR/')
     initFolder(args=args)
 
 def prune_training_set(args):
     # prune HR dataset
-    regions_path =  args.project + '/Permanent/HR/regions/'
-    masks_path = args.project + '/Permanent/HR/masks/'
+    regions_path = args.base_dir + '/' + args.project + '/Permanent/HR/regions/'
+    masks_path = args.base_dir + '/' + args.project + '/Permanent/HR/masks/'
     prune_percent = args.prune_HR
     prune_data(regions_path, masks_path, prune_percent, args)
 
     # prune LR dataset
-    regions_path = args.project + '/Permanent/LR/regions/'
-    masks_path = args.project + '/Permanent/LR/masks/'
+    regions_path = args.base_dir + '/' + args.project + '/Permanent/LR/regions/'
+    masks_path = args.base_dir + '/' + args.project + '/Permanent/LR/masks/'
     prune_percent = args.prune_LR
     prune_data(regions_path, masks_path, prune_percent, args)
 
@@ -41,38 +41,39 @@ def initFolder(args):
     dirs = {'imExt': '.jpeg'}
     dirs['basedir'] = args.base_dir
     dirs['maskExt'] = '.png'
-    dirs['modeldir'] = '/MODELS/'
-    dirs['tempdirLR'] = '/TempLR'
-    dirs['tempdirHR'] = '/TempHR'
-    dirs['pretraindir'] = '/Deeplab_network/'
-    dirs['training_data_dir'] = '/TRAINING_data/'
-    dirs['validation_data_dir'] = '/HOLDOUT_data/'
+    dirs['modeldir'] = 'MODELS'
+    dirs['tempdirLR'] = 'TempLR'
+    dirs['tempdirHR'] = 'TempHR'
+    dirs['pretraindir'] = 'Deeplab_network'
+    dirs['training_data_dir'] = 'TRAINING_data'
+    dirs['validation_data_dir'] = 'HOLDOUT_data'
     dirs['model_init'] = 'deeplab_resnet.ckpt'
-    dirs['project']= args.project
-    dirs['modelfile'] = args.modelfile
-    dirs['data_dir_HR'] = args.project + '/Permanent/HR/'
-    dirs['data_dir_LR'] = args.project + '/Permanent/LR/'
+    dirs['project']= '/' + args.project
+    dirs['data_dir_HR'] = args.base_dir + args.project + 'Permanent/HR'
+    dirs['data_dir_LR'] = args.base_dir + args.project + 'Permanent/LR'
     initializeFolderStructure(dirs,args)
     print('Please add xmls/svs files to the newest TRAINING_data folder.')
 
 
 def initializeFolderStructure(dirs,args):
-    #os.makedirs(dirs['modelfile']+'/yummuy')
-    tmp = os.path.dirname(dirs['project'])
-    print(tmp)
-    os.chdir(tmp)
-    real_path = os.path.realpath(dirs['project'])
-    print(real_path)
-    os.mkdir('MODELS')
-    print('b')
-    os.makedirs(os.path.join(dirs['project'],'test2'))
-    print('c')
-    print(os.listdir())
-    #os.makedirs(dirs['modeldir'])
-    print('d')
-    #make_folder(dirs['project'] + dirs['modeldir'] + str(0) + '/LR/')
-    #make_folder(dirs['project']+ dirs['modeldir'] + str(0) + '/HR/')
 
+    folder_base = args.base_dir
+    base_directory_id = folder_base.split('/')[-2]
+    _ = os.system("printf '\nIn the base directory: {} {}\n'".format(base_directory_id,folder_base))
+
+    folder_project = args.project
+    project_directory_id = folder_project.split('/')[-2]
+    _ = os.system("printf '\nIn the base directory: {}{}\n'".format(project_directory_id,folder_base))
+    
+    gc = girder_client.GirderClient(apiUrl=args.girderApiUrl)
+    gc.setToken(args.girderToken)
+
+    # modeldir = gc.createFolder(project_directory_id, dirs['modeldir'])
+    
+
+    # make_folder(dirs['basedir'] +dirs['project'] + dirs['modeldir'] + str(0) + '/LR/')
+    # make_folder(dirs['basedir'] +dirs['project']+ dirs['modeldir'] + str(0) + '/HR/')
+    
     if args.transfer==' ':
         pass
     else:
@@ -97,47 +98,11 @@ def initializeFolderStructure(dirs,args):
         pretrain_filesLR=glob(dirs['basedir']+ '/' + args.transfer + dirs['modeldir'] + str(modelLast) + '/LR/' + 'model.ckpt-' + str(maxmodelLR) + '*')
         pretrain_filesHR=glob(dirs['basedir']+ '/' + args.transfer + dirs['modeldir'] + str(modelLast) + '/HR/' + 'model.ckpt-' + str(maxmodelHR) + '*')
         for file in pretrain_filesLR:
-            copy(file,dirs['project']+ dirs['modeldir'] + str(0) + '/LR/')
+            copy(file,dirs['basedir'] +dirs['project']+ dirs['modeldir'] + str(0) + '/LR/')
 
         for file in pretrain_filesHR:
-            copy(file,dirs['project']+ dirs['modeldir'] + str(0) + '/HR/')
+            copy(file,dirs['basedir'] +dirs['project']+ dirs['modeldir'] + str(0) + '/HR/')
 
+    training_data_dir = gc.createFolder(project_directory_id, dirs['training_data_dir'])
 
-    make_folder(dirs['training_data_dir'] + str(0))
-
-    make_folder(dirs['tempdirLR'] + '/regions')
-    make_folder(dirs['project']+ dirs['tempdirLR'] + '/masks')
-
-    make_folder(dirs['tempdirLR'] + '/Augment' +'/regions')
-    make_folder(dirs['tempdirLR'] + '/Augment' +'/masks')
-
-    make_folder(dirs['tempdirHR'] + '/regions')
-    make_folder( dirs['tempdirHR'] + '/masks')
-
-    make_folder(dirs['tempdirHR'] + '/Augment' +'/regions')
-    make_folder(dirs['tempdirHR'] + '/Augment' +'/masks')
-
-    make_folder(dirs['modeldir'])
-    make_folder(dirs['training_data_dir'])
-    make_folder(dirs['validation_data_dir'])
-
-
-    make_folder('/Permanent' +'/LR/'+ 'regions/')
-    make_folder('/Permanent' +'/LR/'+ 'masks/')
-    make_folder('/Permanent' +'/HR/'+ 'regions/')
-    make_folder('/Permanent' +'/HR/'+ 'masks/')
-
-    make_folder(dirs['training_data_dir'])
-
-    make_folder(dirs['basedir'] + '/Codes/Deeplab_network/datasetLR')
-    make_folder(dirs['basedir'] + '/Codes/Deeplab_network/datasetHR')
-
-
-def make_folder(directory):
-    #if not os.path.exists(directory):
-    print(directory,'directory')
-    try:
-        print('helllllloooooo')
-        os.makedirs(directory) # make directory if it does not exit already # make new directory # Check if folder exists, if not make it
-    except:
-        print('Folder already exists!')
+    print(training_data_dir, 'this is training data dir')
